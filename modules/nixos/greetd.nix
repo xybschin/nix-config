8e8e81f7ac9ee48d485dc3b5c7acf07d5449979a
xybschin <hello@bjarneschindler.dev>
2026-03-18 22:44:33 +0100

    update

--- modules/nixos/greetd.nix
+++ modules/nixos/greetd.nix
@@ -7,15 +7,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = ''
-          ${pkgs.runtimeShell} -c "
-            ${pkgs.wlr-randr}/bin/wlr-randr \
-              --output DP-1 --mode 2560x1440 --pos 0,0 \
-              --output HDMI-A-1 --mode 2560x1440 --pos 2560,0;
-            
-            "${tuigreet} --time --asterisks --remember --remember-session"
-          "
-        '';
+        command = "${tuigreet} --time --asterisks --remember --remember-session";
         user = "greeter";
       };
     };
