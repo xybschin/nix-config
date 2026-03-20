6d161bb56f2efd95d6af624cfbf3b31639ff8c54
xybschin <hello@bjarneschindler.dev>
2026-03-20 08:46:06 +0100

    update

--- modules/home-manager/desktop/wm/hyprland/binds.nix
+++ modules/home-manager/desktop/wm/hyprland/binds.nix
@@ -68,11 +68,7 @@
       "$mod, 3, workspace, 3"
       "$mod, 4, workspace, 4"
       "$mod, 5, workspace, 5"
-      "$mod, 6, workspace, 6"
-      "$mod, 7, workspace, 7"
-      "$mod, 8, workspace, 8"
-      "$mod, 9, workspace, 9"
-      "$mod, 0, workspace, 10"
+      "$mod, G, workspace, 6"
 
       # Move active window to a workspace.
       "$mod SHIFT, 1, movetoworkspace, 1"
@@ -80,11 +76,7 @@
       "$mod SHIFT, 3, movetoworkspace, 3"
       "$mod SHIFT, 4, movetoworkspace, 4"
       "$mod SHIFT, 5, movetoworkspace, 5"
-      "$mod SHIFT, 6, movetoworkspace, 6"
-      "$mod SHIFT, 7, movetoworkspace, 7"
-      "$mod SHIFT, 8, movetoworkspace, 8"
-      "$mod SHIFT, 9, movetoworkspace, 9"
-      "$mod SHIFT, 0, movetoworkspace, 10"
+      "$mod SHIFT, G, movetoworkspace, 6"
 
       "$mod CTRL SHIFT, l, movetoworkspace, r+1"
       "$mod SHIFT, Right, movetoworkspace, r+1"
