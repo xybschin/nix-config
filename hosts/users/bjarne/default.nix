a4370571b4c4fbc47ba1a331972640bc1e057550
xybschin <hello@bjarneschindler.dev>
2026-03-25 20:39:18 +0100

    update

--- hosts/users/bjarne/default.nix
+++ hosts/users/bjarne/default.nix
@@ -11,6 +11,7 @@
       "wheel"
       "docker"
       "networkmanager"
+      "openrazer"
     ];
     shell = pkgs.zsh;
     hashedPassword = "$y$j9T$NUUdJqm0TLbeSko6tfPww1$RQXYJ.jM17uWDkwmtlssASXcthw4MUo2Y9t.ixw63F9";
