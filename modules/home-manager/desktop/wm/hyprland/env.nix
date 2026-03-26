8df5474ff37d0a9bdf94eb11c5c99a89c17c0c38
xybschin <hello@bjarneschindler.dev>
2026-03-26 22:58:43 +0100

    update

--- modules/home-manager/desktop/wm/hyprland/env.nix
+++ modules/home-manager/desktop/wm/hyprland/env.nix
@@ -3,17 +3,16 @@
 {
   wayland.windowManager.hyprland.settings = {
     env = [
-      "HYPRCURSOR_THEME,bibata-modern-ice"
-      "HYPRCURSOR_SIZE,24"
       "XDG_CURRENT_DESKTOP,Hyprland"
       "XDG_SESSION_TYPE,wayland"
-      "XDG_SESSION_DEKSTOP,Hyprland"
       "QT_QPA_PLATFORM,wayland"
       "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
       "QT_AUTO_SCREEN_SCALE_FACTOR,1"
       "MOZ_ENABLE_WAYLAND,1"
       "XDG_SESSION_TYPE,wayland"
       "HYPRSHOT_DIR,screenshots"
+      "NIXOS_OZONE_WL,1"
+      "WLR_NO_HARDWARE_CURSORS,1"
     ];
   };
 }
