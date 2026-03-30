147df4a4f5582b2910934386b4359367406d6497
xybschin <hello@bjarneschindler.dev>
2026-03-30 11:57:41 +0200

    update

--- modules/home-manager/desktop/wm/hyprland/default.nix
+++ modules/home-manager/desktop/wm/hyprland/default.nix
@@ -2,18 +2,24 @@
   pkgs,
   ...
 }:
+let
+  scripts = builtins.path {
+    path = ./scripts;
+    name = "hprland-scripts";
+  };
+in
 {
   imports = [
     ./env.nix
     ./binds.nix
-    # ./scripts.nix
     ./rules.nix
-    ./settings.nix
-    # ./hyprlock.nix
+    (import ./settings.nix { inherit scripts; })
     ./hyprpaper
   ];
 
   home.packages = with pkgs; [
+    socat
+    jq
     hyprcursor
     hyprshot
     hyprshutdown
