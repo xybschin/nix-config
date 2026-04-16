12630377d40f0040ecac1bc8f24aac114fcb5ae7
= <=>
2026-04-16 13:54:27 +0200

    update

--- modules/home-manager/claude-code.nix
+++ modules/home-manager/claude-code.nix
@@ -1,9 +1,22 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  providersFilePath = "${config.home.homeDirectory}/claude-code-providers.json";
+in
 {
   home.packages = with pkgs; [ claude-code ];
   home.shellAliases.cc = "claude";
 
   programs.claude-code = {
-    enabale = true;
+    enable = true;
+    settings = lib.mkMerge [
+      (lib.mkIf (builtins.pathExists providersFilePath) {
+        provider = builtins.fromJSON (builtins.readFile providersFilePath);
+      })
+    ];
   };
 }
