1cf2c08019a1d91d5d855eaf14429f0b680b0ac0
xybschin <hello@bjarneschindler.dev>
2026-04-16 16:34:45 +0200

    update

--- modules/home-manager/desktop/hyprpaper/default.nix
+++ modules/home-manager/desktop/hyprpaper/default.nix
@@ -3,26 +3,12 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [
-        "~/wallpapers/left.jpg"
-        "~/wallpapers/right.jpg"
-      ];
       splash = false;
       wallpaper = [
         {
-          monitor = "DP-1";
-          path = "~/wallpapers/left.jpg";
-          fit_mode = "contain";
-        }
-        {
-          monitor = "HDMI-A-1";
-          path = "~/wallpapers/right.jpg";
-          fit_mode = "contain";
-        }
-        {
           monitor = "";
-          path = "~/wallpapers/left.jpg";
-          fit_mode = "contain";
+          path = "~/wallpapers/single/artemis-ii-earth.jpg";
+          fit_mode = "cover";
         }
       ];
     };
