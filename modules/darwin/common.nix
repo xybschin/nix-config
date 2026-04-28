392bf3e4ab7700f91474b4aa1aad23fa35dec8b3
= <=>
2026-04-28 11:19:22 +0200

    update

--- modules/darwin/common.nix
+++ modules/darwin/common.nix
@@ -12,6 +12,8 @@
   nix = {
     package = pkgs.nix;
     settings = {
+      substituters = [ "https://claude-code.cachix.org" ];
+      trusted-public-keys = [ "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk=" ];
       trusted-users = [
         "root"
         user
