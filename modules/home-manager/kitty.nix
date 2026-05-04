0789068f5b54e39f780a5a19453cadf2cbd122df
xybschin <hello@bjarneschindler.dev>
2026-05-04 17:01:13 +0200

    update

---
+++ modules/home-manager/kitty.nix
@@ -0,0 +1,59 @@
+{ ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+
+    settings = {
+      foreground = "#ffffff";
+      background = "#101010";
+      selection_foreground = "#b0b0b0";
+      selection_background = "#272727";
+
+      cursor = "#b0b0b0";
+      cursor_text_color = "#101010";
+
+      scrollbar_handle_color = "#b0b0b0";
+      scrollbar_track_color = "#272727";
+
+      url_color = "#d9ba73";
+
+      active_border_color = "#ffffff";
+      inactive_border_color = "#777777";
+      bell_border_color = "#d9ba73";
+
+      wayland_titlebar_color = "system";
+      macos_titlebar_color = "system";
+
+      active_tab_foreground = "#ffffff";
+      active_tab_background = "#101010";
+      inactive_tab_foreground = "#777777";
+      inactive_tab_background = "#272727";
+      tab_bar_background = "#272727";
+
+      mark1_foreground = "#121212";
+      mark1_background = "#b3b3b3";
+      mark2_foreground = "#121212";
+      mark2_background = "#f4b8e4";
+      mark3_foreground = "#121212";
+      mark3_background = "#5abfb5";
+
+      color0 = "#101010";
+      color8 = "#666666";
+      color1 = "#ff5733";
+      color9 = "#ff7676";
+      color2 = "#86cd82";
+      color10 = "#a3d6a3";
+      color3 = "#d9ba73";
+      color11 = "#ffffff";
+      color4 = "#b3b3b3";
+      color12 = "#666666";
+      color5 = "#f2a4db";
+      color13 = "#f4b8e4";
+      color6 = "#5abfb5";
+      color14 = "#fafafa";
+      color7 = "#a5adce";
+      color15 = "#b5bfe2";
+    };
+  };
+}
