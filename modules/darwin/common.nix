f302af37412eaf2fe4c7d2f7dfaf20c31893f2ce
xybschin <hello@bjarneschindler.dev>
2026-05-15 12:34:00 +0200

    update

--- modules/darwin/common.nix
+++ modules/darwin/common.nix
@@ -12,16 +12,8 @@
   nix = {
     package = pkgs.nix;
     settings = {
-      substituters = [
-        "https://claude-code.cachix.org"
-        "https://hyprland.cachix.org"
-        "https://xybschin.cachix.org"
-      ];
-      trusted-public-keys = [
-        "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk="
-        "xybschin.cachix.org-1:Vx/oBQ8RM8ZGhMWEheZvKg75UeB9G2PjitgDIUQ+tYo="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
+      substituters = [ "https://claude-code.cachix.org" ];
+      trusted-public-keys = [ "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk=" ];
       trusted-users = [
         "root"
         user
