5028b7d11b180e30545ff4917423d040c540114f
xybschin <hello@bjarneschindler.dev>
2026-05-18 21:27:32 +0200

    update

--- home-manager/bjarne/nixvidia/hyprland-user/autostart.user.lua
+++ home-manager/bjarne/nixvidia/hyprland-user/autostart.user.lua
@@ -3,6 +3,5 @@ local scriptsDir = os.getenv("HOME") .. "/.config/hypr/scripts"
 -- Autostart on first launch (exec-once)
 hl.on("hyprland.start", function()
 	hl.exec_cmd("xembedsniproxy")
-	hl.exec_cmd("uwsm-app -- 1password --silent")
 	hl.exec_cmd(scriptsDir .. "/monitor-config")
 end)
