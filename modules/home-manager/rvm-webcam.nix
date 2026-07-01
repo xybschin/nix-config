b2467021cabb4d9a85efbda288897d861365a547
xybschin <hello@bjarneschindler.dev>
2026-07-01 16:22:44 +0200

    refactor: use upstream home-manager module from rvm-webcam flake
    The flake now provides homeManagerModules.default. Local module
    just imports it and pre-configures settings, same pattern as
    waybar-audio-control.nix. No option definitions.

--- modules/home-manager/rvm-webcam.nix
+++ modules/home-manager/rvm-webcam.nix
@@ -1,6 +1,5 @@
 { config, pkgs, inputs, ... }:
 let
-  rvmPkg = inputs.rvm-webcam.packages.${pkgs.stdenv.hostPlatform.system}.default;
   rvmModel = pkgs.fetchurl {
     url = "https://github.com/PeterL1n/RobustVideoMatting/releases/download/v1.0.0/rvm_resnet50.pth";
     hash = "sha256-wZGoByURZMBz3OX6QI56gWBw1Tm4grKjFQMwqf7BEs4=";
@@ -8,32 +7,17 @@ let
   };
 in
 {
-  home.packages = [ rvmPkg ];
-
-  systemd.user.services.rvm-webcam = {
-    Unit = {
-      Description = "rvm-webcam background removal virtual camera";
-      After = "graphical-session.target";
-      Wants = "graphical-session.target";
-    };
-    Service = {
-      Type = "simple";
-      ExecStart = "${rvmPkg}/bin/rvm-webcam --on-demand";
-      Restart = "on-failure";
-      RestartSec = "3";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
-
-  xdg.configFile."rvm-webcam/config.json".text = builtins.toJSON {
-    model_path = "${rvmModel}";
+  imports = [ inputs.rvm-webcam.homeManagerModules.default ];
+  services.rvm-webcam = {
+    enable = true;
+    modelPath = "${rvmModel}";
     backbone = "resnet50";
-    bg_image = "/home/bjarne/wallpapers/single/artemis-ii-earth-peek.jpg";
-    downsample_ratio = 0.25;
-    precision = "auto";
-    on_demand = true;
-    compile = true;
+    extraConfig = {
+      bg_image = "/home/bjarne/wallpapers/single/artemis-ii-earth-peek.jpg";
+      downsample_ratio = 0.25;
+      precision = "auto";
+      on_demand = true;
+      compile = true;
+    };
   };
 }
