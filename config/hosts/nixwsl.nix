5a59676353615827691aff9733cfa48bd299a319
xybschin <hello@bjarneschindler.dev>
2026-08-21 23:34:07 +0200

    chore(nixwsl): update theming comment to vesper

--- config/hosts/nixwsl.nix
+++ config/hosts/nixwsl.nix
@@ -53,7 +53,7 @@
       "coding-agents"
     ];
     home.configuration = { pkgs, ... }: {
-      # Enables CLI theming (opencode, zsh, fzf, lazygit, ...) with koda-dark
+      # Enables CLI theming (opencode, zsh, fzf, lazygit, ...) with vesper
       stylix.enable = true;
 
       home.packages =
